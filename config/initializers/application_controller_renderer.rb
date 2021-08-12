81f85ad12752b67ab859b6f3a72a2e6fcbf0d8e6
kengo-hub <shoot.bm@gmeil.com>
2021-08-12 11:16:36 +0000

    add

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '8f8b87ef036995e7c3b24b54179dfaf38305a2884e934cd31c1a2c94bc11cc97a01642c767be55b613324b6e7c8ea012eb771801ec73e4f218fcef64d71a9e69'
