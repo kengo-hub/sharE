4db5b2d3f0f4feae57382d1e22c8c4226ffcdf97
kengo-hub <shoot.bm@gmeil.com>
2021-08-15 14:19:34 +0000

    updateraty

--- app/controllers/public/venues_controller.rb
+++ app/controllers/public/venues_controller.rb
@@ -7,6 +7,11 @@ class Public::VenuesController < ApplicationController
     @venue = Venue.find(params[:id])
     @review = Review.new
     @reviews = Review.all
+    if @venue.reviews.blank?
+      @average_review = 0
+    else
+      @average_review = @venue.reviews.average(:rate).round(2)
+    end
   end
   
   
