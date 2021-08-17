d5ee21c0eb3a04954aa46d9491f635e935696bb1
kengo-hub <shoot.bm@gmeil.com>
2021-08-17 12:41:35 +0000

    add

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,6 +1,24 @@
 class Public::UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:show]
   def show
     @user = User.find(params[:id])
+    @currentUserEntry=Entry.where(user_id: current_user.id)
+    @userEntry=Entry.where(user_id: @user.id)
+    unless @user.id == current_user.id
+      @currentUserEntry.each do |cu|
+        @userEntry.each do |u|
+          if cu.room_id == u.room_id then
+            @isRoom = true
+            @roomId = cu.room_id
+          end
+        end
+      end
+      if @isRoom
+      else
+        @room = Room.new
+        @entry = Entry.new
+      end
+    end
   end
 
   def edit
@@ -32,3 +50,4 @@ class Public::UsersController < ApplicationController
     params.require(:user).permit(:nickname, :profile_image, :introduction, :age)
   end
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_15_143853) do
+ActiveRecord::Schema.define(version: 2021_08_17_083203) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,6 +30,15 @@ ActiveRecord::Schema.define(version: 2021_08_15_143853) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "entries", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "room_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_entries_on_room_id"
+    t.index ["user_id"], name: "index_entries_on_user_id"
+  end
+
   create_table "events", force: :cascade do |t|
     t.integer "venue_id", null: false
     t.integer "artist_id", null: false
@@ -37,6 +46,17 @@ ActiveRecord::Schema.define(version: 2021_08_15_143853) do
     t.date "event_date", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "room_id"
+    t.text "message"
+    t.string "image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_messages_on_room_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "relationships", force: :cascade do |t|
@@ -57,6 +77,13 @@ ActiveRecord::Schema.define(version: 2021_08_15_143853) do
     t.index ["event_id"], name: "index_reviews_on_event_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
     t.index ["venue_id"], name: "index_reviews_on_venue_id"
+  end
+
+  create_table "rooms", force: :cascade do |t|
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_rooms_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
