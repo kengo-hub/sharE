9faa697de070c7aba0f0220b77fc8e9a4474a256
kengo-hub <shoot.bm@gmeil.com>
2021-08-25 06:33:42 +0000

    fix

--- db/migrate/20210817082150_create_entries.rb
+++ db/migrate/20210817082150_create_entries.rb
@@ -1,8 +1,8 @@
 class CreateEntries < ActiveRecord::Migration[5.2]
   def change
     create_table :entries do |t|
-      t.references :user, type: :integer, foreign_key: true
-      t.references :room, type: :integer, foreign_key: true
+      t.references :user, type: :bigint, foreign_key: true
+      t.references :room, type: :bigint, foreign_key: true
       t.timestamps
     end
   end
