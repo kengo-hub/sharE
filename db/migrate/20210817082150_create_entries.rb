7c649b7b79d8719780cbe3210eb16504a0b09468
kengo-hub <shoot.bm@gmeil.com>
2021-08-25 06:15:23 +0000

    fix

--- db/migrate/20210817082150_create_entries.rb
+++ db/migrate/20210817082150_create_entries.rb
@@ -1,6 +1,7 @@
 class CreateEntries < ActiveRecord::Migration[5.2]
   def change
     create_table :entries do |t|
+      t.references :user, type: :bigint, foreign_key: true
       t.references :room, type: :bigint, foreign_key: true
       t.timestamps
     end
--- db/migrate/20210817082715_create_messages.rb
+++ db/migrate/20210817082715_create_messages.rb
@@ -1,6 +1,7 @@
 class CreateMessages < ActiveRecord::Migration[5.2]
   def change
     create_table :messages do |t|
+      t.references :user, type: :bigint, foreign_key: true
       t.references :room, type: :bigint, foreign_key: true
       t.text :message
       t.string :image_id
